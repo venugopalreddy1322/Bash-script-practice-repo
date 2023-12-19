c69663d5cca60e202622c1825754069df503c4c9
venugopalreddy1322 <149612326+venugopalreddy1322@users.noreply.github.com>
2023-12-19 14:21:21 +0100

    Update grep-awk-commands.sh
    typing changes

--- grep-awk-commands.sh
+++ grep-awk-commands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Demo od grep and awk commands - are used to filter the output
-# free - tells the memory details
+# free - tells the free RAM Status(memory) details
 # mt - megabyte Total
 
 free -mt | grep "Total" | awk '{print $1, $4}'
