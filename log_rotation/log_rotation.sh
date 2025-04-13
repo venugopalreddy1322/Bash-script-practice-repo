596a7f24b674dbaa37472946d725837393d08100
Venu_DevOps <venugopalreddy1322@gmail.com>
2025-04-13 22:09:43 +0200

    new additions

--- log_rotation/log_rotation.sh
+++ log_rotation/log_rotation.sh
@@ -4,9 +4,14 @@
 # Purpose: Rotate logs in a directory, compress old logs, and cleanup older backups.
 
 # Check the logs directory exists?, and if not throw an error
-if [[ -d logs ]]; then
-    echo "Logs directory(logs) exists"
+LOG_DIR=logs
+BACKUP_DIR=backup
+if [[ -d $LOG_DIR ]]; then
+    echo "Logs directory($LOG_DIR) exists"
 else
-    echo "ERROR: Log directory(logs) doesn't exists. Exiting."
+    echo "ERROR: Log directory($LOG_DIR) doesn't exists. Exiting."
     exit 1
 fi
+
+# Create the Backup directory if it doesn't exist
+mkdir -p $BACKUP_DIR
